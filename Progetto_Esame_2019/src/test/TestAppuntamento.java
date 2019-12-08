344755e00bb66fe43dcd8ab81822a7c4f78d9d0c
LucaBBB <46371823+LucaBBB@users.noreply.github.com>
2019-12-08 18:18:28 +0100

    Add files via upload

---
+++ Progetto_Esame_2019/src/test/TestAppuntamento.java
@@ -0,0 +1,25 @@
+package test;
+
+import classes.Appuntamento;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestAppuntamento {
+    @Test
+    public void testCreazioneAppuntamento() {
+        String data = "01-01-2019";
+        String ora = "10:00";
+        int durata = 30;
+        String nome = "Pippo";
+        String luogo = "Casa";
+
+        Appuntamento a = new Appuntamento(data, ora, durata, nome, luogo);
+
+        assertEquals(data, a.getDataString());
+        assertEquals(ora, a.getOraString());
+        assertEquals(durata, a.getDurata());
+        assertEquals(nome, a.getNome());
+        assertEquals(luogo, a.getLuogo());
+    }
+}
