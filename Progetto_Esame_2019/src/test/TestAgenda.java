5bf1858ee97e43104bec73ec0937c5c506700b2e
LucaBBB <lucaborsalino98@gmail.com>
2019-12-13 16:16:31 +0100

    Aggiunte varie in tutto il codice

--- Progetto_Esame_2019/src/test/TestAgenda.java
+++ Progetto_Esame_2019/src/test/TestAgenda.java
@@ -2,124 +2,34 @@ package test;
 
 import classes.Agenda;
 import classes.Appuntamento;
+import exception.DurataException;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+/**
+ *
+ */
 public class TestAgenda {
 
-    /**
     Agenda ag;
 
     @Before
     public void init() {
         ag = new Agenda();
-        Appuntamento a = new Appuntamento("10-12-2019", "15:00", 15, "Pippo", "Ambulatorio");
-        Appuntamento b = new Appuntamento("10-12-2019", "14:00", 45, "Pluto", "Ambulatorio");
-        assertEquals(0, ag.addAppuntamento(a));
-        assertEquals(1, ag.addAppuntamento(b));
-    }
-    @Test
-    public void testAggiunta() {
-        System.out.println("Stampa pre testAggiunta()");
-        ag.printListaAppuntamenti();
-        assertEquals(-3, ag.addAppuntamento(new Appuntamento("10-12-2019", "14:50", 15, "Mickey", "Ambulatorio")));
-        assertEquals(2, ag.addAppuntamento(new Appuntamento("11-12-2019", "10:00", 30, "Mickey", "Ambulatorio")));
-
-
-
-        System.out.println("Stampa post testAggiunta()");
-        ag.printListaAppuntamenti();
-        /**
-
-        Agenda ag = new Agenda();
         ag.initAgenda();
-
-        assertEquals(-3, ag.addAppuntamento(c));
-    }
-    */
-
-    /**
-     * Test per l'aggiunta di appuntamenti in rubrica.
-     * Ultimo aggiornamento: 11/12/19 - 11:19.
-     */
-    @Test
-    public void testAggiunta() {
-        Appuntamento a = new Appuntamento("10-12-2019", "15:00", 15, "Pippo", "Ambulatorio");
-        Appuntamento b = new Appuntamento("10-12-2019", "14:00", 45, "Pluto", "Ambulatorio");
-        Appuntamento c = new Appuntamento("10-12-2019", "14:50", 15, "Mickey", "Ambulatorio");
-        Agenda ag = new Agenda();
-        ag.initAgenda();
-        assertEquals(0, ag.addAppuntamento(a));
-        assertEquals(1, ag.addAppuntamento(b));
-        assertEquals(-3, ag.addAppuntamento(c));
-        ag.printListaAppuntamenti();
     }
 
-    /**
-     * Test per l'eliminazione di appuntamenti dalla rubrica.
-     * Ultimo aggiornamento: 11/12/19 - 11:38.
-     */
     @Test
-    public void testRemove() {
-        Appuntamento a = new Appuntamento("10-12-2019", "15:00", 15, "Pippo", "Ambulatorio");
-        Appuntamento b = new Appuntamento("10-12-2019", "14:00", 45, "Pluto", "Ambulatorio");
-        Appuntamento c = new Appuntamento("10-12-2019", "14:50", 15, "Mickey", "Ambulatorio");
-        Agenda ag = new Agenda();
-        ag.initAgenda();
-        assertEquals(0, ag.addAppuntamento(a));
-        assertEquals(1, ag.addAppuntamento(b));
-        assertEquals(-3, ag.addAppuntamento(c));
-        // ... FINO A QUI HO AGGIUNTO a E b ...
-        System.out.println("... Stampa pre eliminazione ...");
-        ag.printListaAppuntamenti();
-
-        assertEquals(1, ag.removeAppuntamento(a));  // RIMUOVO a;
-        assertEquals(-1, ag.removeAppuntamento(c)); // VERIFICO CHE NON CI SIA c DA RIMUOVERE;
-        assertEquals(1, ag.removeAppuntamento(b));  // RIMUOVO b;
-        assertEquals(0, ag.removeAppuntamento(a));  // VERIFICO CHE NON CI SIA PIU' NULLA DA RIMUOVERE.
-        System.out.println("... Stampa post eliminazione ...");
-        ag.printListaAppuntamenti();
-    }
-
-    /**
-     * Test per verificare il corretto funzionamento del metodo findAppuntamento.
-     * Ultimo aggiornamento: 11/12/19 - 11:38.
-     */
-    @Test
-    public void testFindAppuntamento() {
-        Appuntamento a = new Appuntamento("10-12-2019", "15:00", 15, "Pippo", "Ambulatorio");
-        Appuntamento b = new Appuntamento("10-12-2019", "14:00", 45, "Pluto", "Ambulatorio");
-        Appuntamento c = new Appuntamento("10-12-2019", "14:50", 15, "Mickey", "Ambulatorio");
-        Agenda ag = new Agenda();
-        ag.initAgenda();
-        assertEquals(0, ag.addAppuntamento(a));
-        assertEquals(1, ag.addAppuntamento(b));
-        assertEquals(-3, ag.addAppuntamento(c));
-        // ... FINO A QUI HO AGGIUNTO a E b ...
-        assertEquals(a, ag.findAppuntamento(a));                // verifico di trovare a cercando a;
-        assertEquals(null, ag.findAppuntamento(c));    // verifico di trovare null cercando c, non esistendo.
-    }
-
-    /**
-     * Test per verificare il corretto funzionamento del metodo findAppuntamentoByName.
-     * Ultimo aggiornamento: 11/12/19 - 11:53.
-     */
-    @Test
-    public void testFindAppuntamentoByName() {
-        Appuntamento a = new Appuntamento("10-12-2019", "15:00", 15, "Pippo", "Ambulatorio");
-        Appuntamento b = new Appuntamento("10-12-2019", "14:00", 45, "Pluto", "Ambulatorio");
-        Appuntamento c = new Appuntamento("10-12-2019", "14:50", 15, "Mickey", "Ambulatorio");
-        Agenda ag = new Agenda();
-        ag.initAgenda();
-        assertEquals(0, ag.addAppuntamento(a));
-        assertEquals(1, ag.addAppuntamento(b));
-        assertEquals(-3, ag.addAppuntamento(c));
-        //assertEquals(1, ag.addAppuntamento(new Appuntamento("11-12-2019", "10:00", 30, "Pluto", "Ambulatorio")));
-        // ... FINO A QUI HO AGGIUNTO a E b ...
-        ArrayList<Appuntamento> trovati = ag.findAppuntamentoByName("Pluto");  // verifico che ci sia 1 contatto
-        assertEquals(1, trovati.size());                                    // con il nome passato in input.
+    public void testAggiunta() throws DurataException, IOException {
+        //assertEquals(2, ag.readFile("File_Appuntamenti_Test")-1);
+        assertEquals(1, ag.addAppuntamento(new Appuntamento("16-12-2019", "14-00", 30, "Pippo", "Aula_103")));
+        //assertEquals(-1, ag.addAppuntamento(new Appuntamento("16-12-2019", "14-00", 30, "Luca", "Aula_103")));
+        assertEquals(1, ag.addAppuntamento(new Appuntamento("19-12-2019", "14-00", 30, "Giovanni", "Aula_103")));
+        //assertEquals(-1, ag.addAppuntamento(new Appuntamento("13-12-2019", "13-45", 30, "Francesco", "Aula_103")));
+        ag.printOnFile("File_Appuntamenti_Test");
     }
 }
--- Progetto_Esame_2019/src/test/TestAppuntamento.java
+++ Progetto_Esame_2019/src/test/TestAppuntamento.java
@@ -1,19 +1,28 @@
 package test;
 
 import classes.Appuntamento;
+import exception.DurataException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Versione 2.1
+ * Aggiunta dell'eccezione per la durata troppo breve.
+ */
 public class TestAppuntamento {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
     /**
      * Metodo utile per testare la creazione di un oggetto Appuntamento e per verificare il corretto funzionamento
      * dei metodi get della classe Appuntamento.
      */
     @Test
-    public void testCreazioneAppuntamento() {
+    public void testCreazioneAppuntamento() throws DurataException {
         String data = "01-01-2019";
-        String ora = "10:00";
+        String ora = "10-00";
         int durata = 30;
         String nome = "Pippo";
         String luogo = "Casa";
@@ -25,6 +34,22 @@ public class TestAppuntamento {
         assertEquals(durata, a.getDurata());
         assertEquals(nome, a.getNome());
         assertEquals(luogo, a.getLuogo());
+    }
+
+    /**
+     * Test che verifica che venga lanciata l'eccezione per la durata troppo breve di un appuntamento (< 5 minuti).
+     * @throws DurataException Eccezione per la durata troppo breve.
+     */
+    @Test
+    public void testCreazioneAppuntamentoEccezioneDurata() throws DurataException {
+        exception.expect(DurataException.class);
+        exception.expectMessage("Durata dell'appuntamento non valida!");
+        String data = "01-01-2019";
+        String ora = "10-00";
+        int durata = 2;
+        String nome = "Pippo";
+        String luogo = "Casa";
+        new Appuntamento(data, ora, durata, nome, luogo);
     }
 
 }
