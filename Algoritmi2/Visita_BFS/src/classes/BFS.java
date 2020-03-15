94efdd2c853600e30c5f0ca4bc1a9906f8a04bfc
LucaBBB <lucaborsalino98@gmail.com>
2020-03-15 13:40:52 +0100

    Aggiunto primo progetto su visita BFS

---
+++ Algoritmi2/Visita_BFS/src/classes/BFS.java
@@ -0,0 +1,116 @@
+package classes;
+
+import it.uniupo.graphLib.GraphInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BFS {
+    //variabili di istanza
+    GraphInterface grafo; //per memorizzare il grafo su cui si lavora
+    boolean[] scoperto; //per memorizzare i nodi scoperti: scoperti[2]=true se il nodo 2 e' stato scoperto
+    ArrayList<Integer> nodiVisitatiInOrdine; //elenco dei nodi nell'ordine in cui sono stati visitati
+    int[] distanza; //distanza[v] = distanza del nodo v dalla sorgente
+
+    /****************************
+     * Questo e' il costruttore
+     ****************************/
+    public BFS(GraphInterface grafoInInput){
+        this.grafo = grafoInInput;
+    }
+
+    public void initScoperto() {
+        int ordineDelGrafo = grafo.getOrder();
+        scoperto = new boolean[ordineDelGrafo];
+
+        for (int i=0; i<ordineDelGrafo; i++) {
+            scoperto[i] = false;
+        }
+    }
+
+
+    /********
+     *  Il metodo visitaBFS(int sorgente) fa una visita BFS dalla sorgente, ma non restituisce niente:
+     *  modifica i valori delle opportune variabili di istanza
+     */
+    private void visitaBFS(int sorgente) {
+        //qui inizializzate correttamente le variabili di istanza che vi servono
+
+        //la coda puo' essere implementata come una ArrayList di interi
+        ArrayList<Integer> coda = new ArrayList<Integer>();
+
+        coda.add(sorgente);
+        scoperto[0] = true;
+
+        while (!coda.isEmpty()) {
+            int u = coda.remove(0);
+
+            for (int vicino : grafo.getNeighbors(u)) {
+                if (!scoperto[vicino]) {
+                    coda.add(vicino);
+                    scoperto[vicino] = true;
+                }
+            }
+        }
+
+        //per aggiungere un elemento in fondo alla "coda": coda.add(elemento)
+        //per leggere e cancellare il primo elemento coda.remove(0)
+
+        /*********
+         * questo metodo non e' completo,
+         * dovete scrivere la visita BFS
+         * aiutatevi con le istruzioni
+         */
+
+    }
+
+    public ArrayList<Integer> getNodesInOrderOfVisit(int sorgente){
+        nodiVisitatiInOrdine = new ArrayList<>();
+        ArrayList<Integer> coda = new ArrayList<>();
+        scoperto = new boolean[grafo.getOrder()];
+
+        nodiVisitatiInOrdine.add(sorgente);
+        coda.add(sorgente);
+
+        while (!coda.isEmpty()) {
+            Integer u = coda.remove(0);
+
+            for (int vicino : grafo.getNeighbors(u)) {
+                 if (!nodiVisitatiInOrdine.contains(vicino)) {
+                     coda.add(vicino);
+                     nodiVisitatiInOrdine.add(vicino);
+                     scoperto[vicino] = true;
+                 }
+            }
+        }
+
+        return nodiVisitatiInOrdine;
+    }
+
+
+    public int[] getDistance(int sorgente) { //resituisce le distanza di ciascun nodo da sorg
+        int[] distanza = new int[grafo.getOrder()];
+        scoperto = new boolean[grafo.getOrder()];
+
+        ArrayList<Integer> Coda = new ArrayList<>();
+        ArrayList<Integer> Scoperti = new ArrayList<>();
+        Scoperti.add(sorgente);
+
+        Arrays.fill(distanza, -1);
+
+        while (!Coda.isEmpty()) {
+            int u = Coda.remove(0);
+            for (int v : grafo.getNeighbors(u)) {
+                if (!Scoperti.contains(v)) {
+                    Coda.add(v);
+                    scoperto[v] = true;
+                    distanza[v] = distanza[u]+1;
+                }
+            }
+        }
+
+        return distanza;
+    }
+
+
+}
---
+++ Algoritmi2/Visita_BFS/src/tests/BFSTest.java
@@ -0,0 +1,103 @@
+package tests;
+
+import classes.BFS;
+import it.uniupo.graphLib.DirectedGraph;
+import it.uniupo.graphLib.GraphInterface;
+import it.uniupo.graphLib.UndirectedGraph;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class BFSTest {
+
+    @Test
+    public void testCreate() {
+        GraphInterface grafo = new DirectedGraph(3);
+        BFS bfsTest = new BFS(grafo);
+        assertNotNull(bfsTest);
+    }
+
+    /**
+     * Metodo che testa se su un grafo con un solo nodo
+     * l'algoritmo di visita restituisca un array di di lunghezza pari a uno.
+     */
+    @Test
+    public void testScoperti1() {
+        GraphInterface grafo = new UndirectedGraph(1);
+        BFS bfsTest = new BFS(grafo);
+        assertEquals(1, bfsTest.getNodesInOrderOfVisit(0).size());
+    }
+
+    /**
+     * Metodo che testa se su un grafo non orientato con due nodi
+     * l'algoritmo di visita restituisca un array di lunghezza pari a due.
+     */
+    @Test
+    public void testScoperti2() {
+        GraphInterface grafo = new UndirectedGraph("2;0 1");
+        BFS bfsTest = new BFS(grafo);
+        assertEquals(2, bfsTest.getNodesInOrderOfVisit(0).size());
+    }
+
+    /**
+     * Metodo che testa su un grafo non orientato generico con 4 nodi
+     * che l'algoritmo restituisca un array di lunghezza pari a quattro.
+     */
+    @Test
+    public void testScopertiGenerico() {
+        GraphInterface grafo = new UndirectedGraph("4;0 2;0 1;2 3;1 3");
+        BFS bfsTest = new BFS(grafo);
+        assertEquals(4, bfsTest.getNodesInOrderOfVisit(2).size());
+    }
+
+    /**
+     * Metodo che testa il corretto funzionamento dell'algoritmo di visita.
+     */
+    @Test
+    public void testBFSOrder() {
+        GraphInterface grafo = new UndirectedGraph("4;0 2;0 1;2 3;1 3");
+        BFS bfsTest = new BFS(grafo);
+        assertTrue(bfsTest.getNodesInOrderOfVisit(2).get(0) == 2);
+        assertTrue(bfsTest.getNodesInOrderOfVisit(2).get(1) == 0);
+        assertTrue(bfsTest.getNodesInOrderOfVisit(2).get(2) == 3);
+        assertTrue(bfsTest.getNodesInOrderOfVisit(2).get(3) == 1);
+        assertFalse(bfsTest.getNodesInOrderOfVisit(2).get(2) == 1);
+    }
+
+    /**
+     * Metodo che testa la lunghezza dopo le due chiamate.
+     */
+    @Test
+    public void testInitNumeroNodiVisitati() {
+        GraphInterface grafo = new UndirectedGraph ("4;0 2;0 1;2 3;1 3");
+        BFS bfsTest = new BFS(grafo); //<<- creato una volta sola
+        int numeroNodi = bfsTest.getNodesInOrderOfVisit(0).size();//<<-prima chiamata del metodo
+        assertEquals(4, numeroNodi);
+        numeroNodi = bfsTest.getNodesInOrderOfVisit(2).size(); //<<-seconda chiamata, stesso oggetto,
+
+        assertEquals(4, numeroNodi);
+    }
+
+    /**
+    @Test
+    public void testDistanza() {
+        GraphInterface grafo = new UndirectedGraph("4;0 2;0 1;2 3;1 3");
+        BFS bfsTest = new BFS(grafo);
+
+    }*/
+
+    public void printArray(ArrayList<Integer> daStampare) {
+        for (Integer integer : daStampare) {
+            System.out.print(integer + " ");
+        }
+        System.out.println("");
+    }
+
+    public void printDistanze(int[] distanza) {
+        for (int i=0; i<distanza.length; i++) {
+            System.out.println("distanza[" + i + "] = " + distanza[i]);
+        }
+    }
+}
