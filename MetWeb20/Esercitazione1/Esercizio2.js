7738fe61957fba99f96bb1a244da076f9d4ae98c
LucaBBB <lucaborsalino98@gmail.com>
2020-03-24 14:24:23 +0100

    Completato esercizio 2 di metweb20 dell'Esercitazione1

--- MetWeb20/Esercitazione1/Esercizio2.js
+++ MetWeb20/Esercitazione1/Esercizio2.js
@@ -4,20 +4,23 @@ const corsi = "Metodologie di programmazione per il Web, Reti 1, Paradigmi di pr
 let arrayDiCorsi = corsi.split(','); // inserisco in arrayDiCorsi ogni corso, separando ciascuno grazie alla virgola.
 let arrayAcronimi = []; //creo l'array per gli acronimi.
 
-for (let i=0; i<arrayDiCorsi.length; i++) {
+let i=0;
+for (const c of arrayDiCorsi) {
     let acronimo = "";
-    let corso = arrayDiCorsi[i].trim(); // prendo un elemento dall'array dei corsi (lo chiamo corso) e tolgo tutti gli spazi inutili.
+    let corso = c.trim(); // prendo un elemento dall'array dei corsi (lo chiamo corso) e tolgo tutti gli spazi inutili.
     let arr = corso.split(' '); // metto in un array (arr) ogni parola di corso splittandolo grazie allo spazio tra le parole.
 
-    for (let j=0; j<arr.length; j++) {
-        acronimo += arr[j].charAt(0); // prendo il primo carattere di ogni parola del corso.
+    for (const a of arr) {
+        acronimo += a.charAt(0); // prendo il primo carattere di ogni parola del corso.
     }
-    arrayAcronimi[i] = acronimo.toUpperCase(); // metto maiuscolo ogni carattere dell'acronimo.
-    arrayDiCorsi[i] = corso;
+    arrayDiCorsi[i] = corso;    // aggiungo il corso senza spazi e a posto
+    arrayAcronimi.push(acronimo.toUpperCase()); // metto maiuscolo ogni carattere dell'acronimo.
+    i++;
 }
 
-arrayAcronimi.sort(); // ordino gli acronimi.
+
 arrayDiCorsi.sort();  // ordino i corsi.
+arrayAcronimi.sort(); // ordino gli acronimi.
 
 for (let i=0; i<arrayDiCorsi.length; i++) {
     console.log(arrayAcronimi[i] + " : " + arrayDiCorsi[i]);
