d3e2a9d94eea2398b68f19cf9587bfd31fbd62dc
LucaBBB <lucaborsalino98@gmail.com>
2020-04-03 11:30:57 +0200

    Esercizio1 non funzionante

--- MetWeb20/Esercitazione2/ESERCIZIO1.js
+++ MetWeb20/Esercitazione2/ESERCIZIO1.js
@@ -1,27 +1,64 @@
-let arrayOfTasks = [{descrizione: "dormire", importanza: "importante", condivisione: "privato", scadenza: "01/04/2020"}];
+"use strict";
 
-function AddNewTask(descrizione, importanza, condivisione, scadenza) {
-    let task = {
-        descrizione: descrizione.toLowerCase(),
-        importanza: importanza.toLowerCase(),
-        condivisione: condivisione.toLowerCase(),
-        scadenza: scadenza.toLowerCase()
-    };
-    arrayOfTasks.push(task);
+let arrayOfTasks = [];
+
+const readlineSync = require('readline-sync');
+
+function showMenu() {
+    console.log("... Menu ...");
+    console.log("... premere 1 per inserire un nuovo task ...");
+    console.log("... premere 2 per rimuovere un task ...");
+    console.log("... premere 3 per rimuovere un task per giorno ...");
+    console.log("... premere 4 per visualizzare tutti i task ...");
+    return parseInt(readlineSync.question("Inserisci il numero riferito alla scelta che vuoi effettuare: "));
 }
 
-/**
- * @return {boolean}
- */
-function RemoveTask(descrizione) {
-    for (let i=0; i<arrayOfTasks.length; i++) {
-        let task = arrayOfTasks[i];
-        if (task.descrizione === descrizione) {
-            arrayOfTasks.splice(i, 1);
-            return true;
+function addNewTask(descr, imp="non importante", cond="privato", scad) {
+    let newTask = {
+        descrizione: descr.toLowerCase(),
+        importanza: imp.toLowerCase(),
+        condivisione: cond.toLowerCase(),
+        scadenza: scad
+    };
+    arrayOfTasks.push(newTask)
+}
+
+function removeTask() {
+    if (arrayOfTasks.length === 0) {
+        console.log("Impossibile rimuovere!");
+        console.log("Nesson task salvato!");
+        return 0;
+    }
+    else {
+        let descrizione = readlineSync.question("Inserire la descrizione del task da rimuovere: ");
+        for (const task of arrayOfTasks) {
+            if (task.descrizione === descrizione)
+                arrayOfTasks.splice(task);
         }
     }
-    return false;
+}
+
+function removeTaskByDate() {
+    if (arrayOfTasks.length === 0) {
+        console.log("Impossibile rimuovere!");
+        console.log("Nessun task salvato!");
+        return -1;
+    }
+    else {
+        let i = 0;
+        let rimossi = 0;
+        while (i < arrayOfTasks.length) {
+            let data = new Date(readlineSync.question("Inserire la data dei task da rimuovere: "));
+            let dataTask = Date.parse(arrayOfTasks[i].scadenza);
+            if (data === dataTask) {
+                arrayOfTasks.splice(arrayOfTasks[i], 1);
+                rimossi++;
+            }
+            else
+                i++;
+        }
+        return rimossi;
+    }
 }
 
 function compare(a, b) {
@@ -34,18 +71,67 @@ function compare(a, b) {
     return 0;
 }
 
-function ListSortedTasks() {
-    arrayOfTasks.sort(compare);
-    console.log("... Stampa dei task in ordine alfabetico ...");
-    console.log(arrayOfTasks);
-}
-
-function Menu() {
-    console.log("... Menu del software ...");
-    let scelta = readLineSync.question("Premere 1 per aggiungere un nuovo task...")
-    if (scelta === 1) {
-        AddNewTask("mangiare", "importante", "pubblico", "31/03/2020");
-        console.log(arrayOfTasks);
+function listSortedTasks() {
+    if (arrayOfTasks.length === 0)
+        console.log("Nessun task presente!");
+    else {
+        arrayOfTasks.sort(compare);
+        console.log("... Stampa dei task in ordine alfabetico ...");
+        for (const task of arrayOfTasks)
+            console.log(task);
     }
 }
-Menu();
+
+function insertData(scelta) {
+    if (scelta === 1) {
+        let descr = readlineSync.question("Inserisci la descrizione del nuovo task: ");
+        let imp;
+        if (readlineSync.question("E' importante (si/no): ") === "si")
+            imp = "importante";
+        else
+            imp = "non importante";
+        let cond = readlineSync.question("E' privato o condiviso?: ");
+        let scad = new Date(readlineSync.question("Inserisci la scadenza del nuovo task (YYYY-MM-DD): "));
+        if (cond === '')
+            addNewTask(descr, imp, scad);
+        else
+            addNewTask(descr, imp, cond, scad);
+    }
+}
+
+let scelta = 0;
+while (true) {
+    do {
+        scelta = showMenu();
+    }
+    while (scelta < 1 || scelta > 4);
+
+    switch(scelta) {
+        case 1:
+            insertData(1);
+            break;
+
+        case 4:
+            listSortedTasks();
+            break;
+
+        /**
+         case 2:
+         removeTask();
+         break;
+
+         case 3:
+         console.log("... Inserimento nuovo task ...");
+
+         break;
+
+         case 4:
+         listSortedTasks();
+         break;
+
+         case 5:
+         console.log("Arrivederci!");
+         break;
+         }*/
+    }
+}
